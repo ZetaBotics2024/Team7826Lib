ba20c389abf18869fe50a755439606d0efd1e1b5
Jetvac2 <Jetvac2@icloud.com>
2024-07-13 20:05:35 -0400

    Set up advantage kit following: https://github.com/Mechanical-Advantage/AdvantageKit/blob/main/docs/INSTALLATION.md

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Team7826Lib";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 3;
+  public static final String GIT_SHA = "7054621d9b5ee5cdb2919ae2d3c87b9d0ae311e5";
+  public static final String GIT_DATE = "2024-07-13 19:31:25 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2024-07-13 19:56:16 EDT";
+  public static final long BUILD_UNIX_TIME = 1720914976911L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,7 +14,7 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * "declarative" paradigm, very little robot logic should actually bPe handled in the {@link Robot}
  * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */

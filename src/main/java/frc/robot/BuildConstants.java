e1a955a8741803acf170b3628314d1dec663ba2e
Jetvac2 <Jetvac2@icloud.com>
2024-08-11 17:54:29 -0400

    Fixed file naming to be uppercase and alliene with packege decloratoins

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Team7826Lib";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 34;
-  public static final String GIT_SHA = "56705e4a99689e47b203b1a7a079d3fb91cd577e";
-  public static final String GIT_DATE = "2024-08-04 18:49:03 EDT";
+  public static final int GIT_REVISION = 35;
+  public static final String GIT_SHA = "2b2d0676c86e1f48026cbe713e1746734f58bd0f";
+  public static final String GIT_DATE = "2024-08-08 18:57:19 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-08-08 18:40:28 EDT";
-  public static final long BUILD_UNIX_TIME = 1723156828809L;
+  public static final String BUILD_DATE = "2024-08-11 17:47:32 EDT";
+  public static final long BUILD_UNIX_TIME = 1723412852646L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}

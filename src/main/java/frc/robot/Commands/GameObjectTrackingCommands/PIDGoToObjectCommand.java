b6ab108b0428624c62550a7c4afa44afbea37957
Jetvac@ <jetvac2@icloud.com>
2024-11-08 13:18:03 -0800

    Refactored Object Tracking and added TODO for it

--- src/main/java/frc/robot/Commands/GameObjectTrackingCommands/PIDGoToObjectCommand.java
+++ src/main/java/frc/robot/Commands/GameObjectTrackingCommands/PIDGoToObjectCommand.java
@@ -1,4 +1,4 @@
-package frc.robot.Commands.AutonCommands.ObjectCommands;
+package frc.robot.Commands.GameObjectTrackingCommands;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;

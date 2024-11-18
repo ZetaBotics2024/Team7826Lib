13a39dcbfad89d1b81deb0fc4210cc7da75c1bd2
DedFishy <boynegregg312@gmail.com>
2024-11-18 18:11:20 -0500

    Applied math to DesiredMetersPerSecondToVoltage

--- src/main/java/frc/robot/Utils/SwerveDriveUtils/DesiredMetersPerSecondToVoltage.java
+++ src/main/java/frc/robot/Utils/SwerveDriveUtils/DesiredMetersPerSecondToVoltage.java
@@ -1,8 +1,28 @@
 package frc.robot.Utils.SwerveDriveUtils;
 
+/*
+ * Utility to use statistical analysis to approximate the voltage required for our motors to achieve a specific velocity.
+ * Our data and regression calculations are in Desmos: https://www.desmos.com/calculator/o8uayiat4b
+ */
 public class DesiredMetersPerSecondToVoltage {
     
-    public static double metersPerSecondToVoltage() {
-        return 0.0;
+    public static final double minSpeedMPS = 0.1679;
+    public static final double maxSpeedMPS = 4.37175;
+
+    public static final double voltageAtMinSpeed = 0;
+    public static final double voltageAtMaxSpeed = 12.12027;
+
+    public static double metersPerSecondToVoltageRegression(double mps) {
+        return 2.734 * mps + 0.1679;
+    }
+
+    public static double metersPerSecondToVoltage(double desiredMetersPerSecond) {
+        return (
+            (desiredMetersPerSecond < minSpeedMPS) ? // If speed is less than the minimum (governed by static friction)...
+            voltageAtMinSpeed : // ...return voltage at minimum speed
+            (desiredMetersPerSecond > maxSpeedMPS) ? // If speed is greater than the maximum (governed by motor quality)...
+            voltageAtMaxSpeed : // ..return voltage at maximum speed
+            metersPerSecondToVoltageRegression(desiredMetersPerSecond) // Return the result of our regression line
+        );
     }
 }

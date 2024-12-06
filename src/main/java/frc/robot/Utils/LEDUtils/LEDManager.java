9d9a868c7bbf9bf4e7a3999c2283897f88538f01
jetvac2 <jetvac2@icloud.com>
2024-12-06 13:19:48 -0500

    Moved back to CANdle LED system

--- src/main/java/frc/robot/Utils/LEDUtils/LEDManager.java
+++ src/main/java/frc/robot/Utils/LEDUtils/LEDManager.java
@@ -1,50 +1,77 @@
 package frc.robot.Utils.LEDUtils;
 
-import edu.wpi.first.wpilibj.AddressableLED;
-import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import org.littletonrobotics.junction.Logger;
+
+import com.ctre.phoenix.led.CANdle;
+import com.ctre.phoenix.led.CANdle.LEDStripType;
+import com.ctre.phoenix.led.CANdle.VBatOutputMode;
+import com.ctre.phoenix.led.CANdleConfiguration;
+import com.ctre.phoenix.led.FireAnimation;
+import com.ctre.phoenix.led.RainbowAnimation;
+import com.ctre.phoenix.led.TwinkleAnimation;
+import com.ctre.phoenix.led.TwinkleAnimation.TwinklePercent;
+
 import frc.robot.Constants.LEDConstants;
-import frc.robot.Constants.RobotModeConstants;
-import frc.robot.Constants.RobotModeConstants.Mode;
 
 public class LEDManager {
-    private static AddressableLED LEDStrip;
-    private static AddressableLEDBuffer LEDBuffer;
 
+    private static CANdle candle;
+    private static CANdleConfiguration candleConfiguration;
+
+    /**
+     * Must be called above the creation of the RobotContainer in the Robot.java robotInit() method but after the all of the Advantage Kit set up. This initalized the LEDs. 
+     * This configures the Candle
+     */
     public static void init() {
-        if(RobotModeConstants.currentMode == Mode.REAL) {
-            LEDStrip = new AddressableLED(LEDConstants.kLEDPortID);
-            LEDBuffer = new AddressableLEDBuffer(LEDConstants.kLEDCount);
-            LEDStrip.setLength(LEDBuffer.getLength());
-            LEDStrip.setData(LEDBuffer);
-            LEDStrip.start(); 
-        }
+        candle = new CANdle(LEDConstants.kLEDPortID);
+        candleConfiguration = new CANdleConfiguration();
+        candleConfiguration.statusLedOffWhenActive = false;
+        candleConfiguration.disableWhenLOS = true; // This was false last year
+        candleConfiguration.stripType = LEDStripType.RGB;
+        candleConfiguration.vBatOutputMode = VBatOutputMode.Modulated;
+        candle.configAllSettings(candleConfiguration);
     }
 
-    public static void setSolidColor(int[] rgbColor) {
-        if(RobotModeConstants.currentMode == Mode.REAL) {
-            for(int i = 0; i < LEDBuffer.getLength(); i++) {
-                LEDBuffer.setRGB(i, rgbColor[0],
-                rgbColor[1],
-                rgbColor[2]);
-            }
-            LEDStrip.setData(LEDBuffer);
-        }
+    /**
+     * Sets the LED light strips on the robot to one solid color
+     * @param rgb Integer Array(length 3), RGB color to set the LEDs to. 
+     */
+    public static void setSolidColor(int[] rgb) {
+        Logger.recordOutput("LED/Color", rgb);
+        candle.setLEDs(rgb[0], rgb[1], rgb[2]);
     }
 
-    public static void setPatern(int[] firstRGBColor, int[] secondRGBColor, int sectionLength) {
-        if(RobotModeConstants.currentMode == Mode.REAL) {
-            int numberOfSections = (int)(LEDBuffer.getLength() / sectionLength);
-            for(int i = 0; i < numberOfSections; i++) {
-                for(int j = 0; j < sectionLength; j++) {
-                    if(i % 2 == 1) {
-                        LEDBuffer.setRGB(i, firstRGBColor[0], firstRGBColor[1], firstRGBColor[2]);
-                    } else {
-                        LEDBuffer.setRGB(i, secondRGBColor[0], secondRGBColor[1], secondRGBColor[2]);
-                    }
-                }
-            }
-            LEDStrip.setData(LEDBuffer);
-        }
+    /**
+     * Sets the LEDs to a "fire animation" based on the parameters passed in.
+     * @param brightness Double: Brightness of the LEDs(0-1)
+     * @param speed Double: The speed at wich the flame will process(0-1)
+     * @param sparkingRate Double: The rate at which sparks are created(0-1)
+     * @param coolingRateDouble: The rate at which the fire fades(0-1)
+     */
+    public static void fireAnimation(double brightness, double speed, double sparkingRate, double coolingRate) {
+        Logger.recordOutput("LED/Color", "Fire Animation");
+        candle.animate(new FireAnimation(brightness, speed, LEDConstants.kLEDCount, sparkingRate, coolingRate));
     }
-    
+
+    /**
+     * Sets the LEDs to do a rainbow animation
+     * @param brightness Double: Brightness of the LEDs(0-1)
+     * @param travelSpeed Double: The travel speed of the rainbow(0-1)
+     */
+    public void rainbowAnimation(double brightness, double travelSpeed) {
+        Logger.recordOutput("LED/Color", "Rainbow Animation");
+        candle.animate(new RainbowAnimation(1, 1, LEDConstants.kLEDCount));
+    }
+
+    /**
+     * Sets the LEDs to a Twinkle Animation
+     * @param rgbw Integer(Length 4): The RGBW values of the LEDs
+     * @param travelSpeed Double: The speed at which the Twinkle animation "twingles"
+     */
+    public void TwinkleAnimation(int[] rgbw, double travelSpeed) { 
+        Logger.recordOutput("LED/Color", "Twinkle Animation");
+        candle.animate(new TwinkleAnimation(rgbw[0], rgbw[1], rgbw[2], rgbw[3], travelSpeed, LEDConstants.kLEDCount, TwinklePercent.Percent100));
+    }
+
+
 }
